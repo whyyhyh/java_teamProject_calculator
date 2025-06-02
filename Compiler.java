574844bf6106263f53d8a156b4d86f4da1888af6
AnDongKyun1103 <asd20361103@gmail.com>
2025-06-02 13:29:00 +0900

    Add files via upload
    안동균

--- Compiler.java
+++ Compiler.java
@@ -1,45 +1,67 @@
-package pck01;
 
 public class Compiler {
-
-    private Register[] memory;
-    private int commandIndex = 0;      
-    private int dataIndex = 15;   
-    private Bus bus;
-
-    public Compiler(Register[] memory, Bus bus) {
-        this.memory = memory;
-        this.bus = bus;
-    }
-
-    public void compile(String expression) {
-        String[] tokens = expression.split(" "); 
-
-        String leftOperand = tokens[0];
-        String operator = tokens[1];
-        String rightOperand = tokens[2];
-
-        int leftDataAddr = dataIndex--;
-        int rightDataAddr = dataIndex--;
-        int resultAddr = dataIndex--;
-
-        bus.compToReg(leftOperand, memory[leftDataAddr]);
-        bus.compToReg(rightOperand, memory[rightDataAddr]);
-
-        bus.compToReg("load " + leftDataAddr, memory[commandIndex++]);
-        bus.compToReg(opToCommand(operator) + " " + rightDataAddr, memory[commandIndex++]);
-        bus.compToReg("store " + resultAddr, memory[commandIndex++]);
-
-        System.out.println("\n[컴파일 완료] 결과는 memory[" + resultAddr + "]에 저장됩니다.");
-    }
-
-    private String opToCommand(String op) {
-        return switch (op) {
-            case "+" -> "add";
-            case "-" -> "sub";
-            case "*" -> "mul";
-            case "/" -> "div";
-            default -> throw new IllegalArgumentException("지원되지 않는 연산자: " + op);
-        };
-    }
+	//private String operrationExpression;
+	private String lodeData;
+	private String storeData;
+	private String operationData;
+	private int operand_1, operand_2;
+	
+	public Compiler() {
+//		//this.operrationExpression = operationExpression;
+//		//char letter;
+//		
+//		String[] splited = operationExpression.split("[+\\-*/ ]+");
+//		value_1 = Integer.parseInt(splited[0]);
+//		value_2 = Integer.parseInt(splited[1]);
+//		for (int i = 0; i < operationExpression.length(); i++) {
+//			if(operationExpression.charAt(i) == '+') {
+//				operationData = "ADD 19";
+//			}
+//			else if(operationExpression.charAt(i) == '-') {
+//				operationData = "SUB 19";
+//			}
+//			else if(operationExpression.charAt(i) == '/') {
+//				operationData = "DIV 19";
+//			}
+//			else if(operationExpression.charAt(i) == '*') {
+//				operationData = "MUL 19";
+//			}
+//		}
+//		lodeData = "LOAD 18";
+//		storeData = "STORE 20";
+		
+	}
+	public String getLodeData() {
+		return lodeData;
+	}
+	public String getstoreData() {
+		return storeData;
+	}
+	public String getoperationData() {
+		return operationData;
+	}
+	public int getOperand_1() {
+		return operand_1;
+	}
+	public int getOperand_2() {
+		return operand_2;
+	}
+	void change(String operationExpression) {
+		String[] splited = operationExpression.split("[+\\-*/ ]+");		
+		operand_1 = Integer.parseInt(splited[0]);		
+		operand_2 = Integer.parseInt(splited[1]);		
+		for (int i = 0; i < operationExpression.length(); i++) {		
+			if(operationExpression.charAt(i) == '+') {			
+				operationData = "ADD 19";			}		
+			else if(operationExpression.charAt(i) == '-') {			
+				operationData = "SUB 19";			}		
+			else if(operationExpression.charAt(i) == '/') {			
+				operationData = "DIV 19";			}		
+			else if(operationExpression.charAt(i) == '*') {			
+				operationData = "MUL 19";			}	
+			}	
+		lodeData = "LOAD 18";	
+		storeData = "STORE 20";
+	}
+	
 }
